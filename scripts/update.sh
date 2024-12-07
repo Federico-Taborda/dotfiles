efcc166356736d316f8bde58d1ecc091214bb2b2
Federico-Taborda <panterx.95@gmail.com>
2024-12-07 10:58:07 -0300

    Clear added

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,16 +4,19 @@
 echo "\n"
 echo "<--Actualizando sistema-->"
 sudo apt update && sudo apt upgrade -y
+clear
 
 # Borra paquetes obsoletos 
 echo "\n"
 echo "<--Eliminando paquetes obsoletos-->"
 sudo apt-get autoremove && sudo apt-get clean
+clear
 
 # Libera espacio en disco
 echo "\n"
 echo "<--Liberando espacio en disco-->"
 rm -rf ~/.cache/thumbnails/*
+clear
 
 echo "\n"
 echo "<--Sistema actualizado y limpio-->"
