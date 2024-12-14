74bb41519338882ff70b709cc9853ce9e75bcd41
Federico-Taborda <panterx.95@gmail.com>
2024-12-13 22:00:47 -0300

    xfce-panel conf added

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,16 +3,22 @@
 # Create backup folder
 mkdir -p $HOME/Escritorio/backup
 mkdir -p $HOME/Escritorio/backup/config
+mkdir -p $HOME/Escritorio/backup/config/xfce4/xfconf/xfce-perchannel-xml
 mkdir -p $HOME/Escritorio/backup/zsh
 
 # Copy zsh configurations
 cd ~
-cp -r .zshrc $HOME/Escritorio/backup/zsh
-cp -r .zsh_aliases $HOME/Escritorio/backup/zsh
+cp .zshrc $HOME/Escritorio/backup/zsh
+cp .zsh_aliases $HOME/Escritorio/backup/zsh
 
-# Copy all configurations
+# Copy all configurations apps
 cd ~/.config
 cp -r ulauncher $HOME/Escritorio/backup/config
 cp -r warp-terminal $HOME/Escritorio/backup/config
 cp -r neofetch $HOME/Escritorio/backup/config
 
+# Copy panel
+cp xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml $HOME/Escritorio/backup/config/xfce4/xfconf/xfce-perchannel-xml
+
+# ZIP backup
+tar -czvf $HOME/Escritorio/backup.tar.gz $HOME/Escritorio/backup
