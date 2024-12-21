1bff055b226761be878e54bfe470c7b18423bb77
Federico-Taborda <panterx.95@gmail.com>
2024-12-21 16:38:30 -0300

    more apps added

--- README.md
+++ README.md
@@ -4,14 +4,9 @@ My dotfiles repository
 # ToDo
 
 # Add the following facilities
-Install papirus icon theme
-Install tldr
-Install btop
 Install nodejs
 Install vscode
-Install valgrind
 Install npm
-Install brave
 
 # Update the following facilities
 Update nodejs
--- install.sh
+++ install.sh
@@ -15,25 +15,47 @@ sudo apt install neofetch
 # <-- Development applications -->
 sudo apt update
 
-# Python test runner
-pip install -U pytest
-
 # Python package manager
 sudo apt install python3-pip
 
+# Python test runner
+pip install -U pytest
+
+# TLDR Pages
+sudo apt install tldr
+tldr -u
+
+# <-- Applications -->
+
+# Install Brave browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
 # <-- Customization applications -->
 
+# Icon theme
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install papirus-icon-theme
+
 # CLI time application
-#sudo apt install tty-clock
+sudo apt install tty-clock
 
 # Visualizer audio application
+#sudo apt install build-essential libfftw3-dev libasound2-dev libpulse-dev libtool automake libiniparser-dev libsdl2-2.0-0 libsdl2-dev libpipewire-0.3-dev libjack-jackd2-dev pkgconf
 #sudo apt install cava
 
 # Color scheme generator
 #sudo apt install imagemagick imagemagick-6.q16
 #sudo pip3 install pywal
  
-
 # Application searchers
 #sudo apt install rofi
 #sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt install ulauncher
+
+# <-- System applications -->
+
+# System monitor
+sudo apt install btop
+
+# Memory debugging tool
+sudo apt install valgrind
